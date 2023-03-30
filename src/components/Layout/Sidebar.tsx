404de38088b1701a8e5d52143bd884ecbc5b9733
Dlsmiuel <migueldls.mb@gmail.com>
2023-03-30 10:24:12 -0400

    first commit, finished layout

---
+++ src/components/Layout/Sidebar.tsx
@@ -0,0 +1,43 @@
+import { useRouter } from "next/router";
+import { FC } from "react";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import { BiLogOut } from "react-icons/bi";
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+import SidebarTweetButton from "./SidebarTweetButton";
+interface SidebarProps {}
+
+const Sidebar: FC<SidebarProps> = ({}) => {
+  const items = [
+    { label: "Home", href: "/", icon: BsHouseFill },
+    { label: "Notifications", href: "/notifications", icon: BsBellFill },
+    { label: "Profile", href: "/users/123", icon: FaUser },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px] ">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem
+            onClick={() => {}}
+            icon={BiLogOut}
+            label="logout"
+            href=""
+          />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
